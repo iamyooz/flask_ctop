d6d9e43ef312edd33b4174f6d37014e3f5a2799d
unknown <yujin23@sookmyung.ac.kr>
2023-08-28 21:03:25 +0900

    Update Flask app

--- ctop.py
+++ ctop.py
@@ -15,26 +15,28 @@ scaler = StandardScaler()
 def home():
     return "hi"
 
-@app.route('/casusal_top', methods=['GET', 'POST'])
+@app.route('/casu', methods=['POST'])
 def cluster_data():
-    if request.method == 'POST':
-        try:
+    try:
+        if request.method == 'POST' and request.is_json:
             data = request.json  # 클라이언트에서 전송한 JSON 데이터
             new_data = np.array([[data['평균기온(°C)'], data['일교차(°C)']]])
-        
+
             # 입력 데이터 표준화
             new_data_scaled = scaler.transform(new_data)
-        
+
             # 클러스터 예측
             cluster_label = kmeans_model.predict(new_data_scaled)[0]
-        
+
             response = {
                 'cluster': int(cluster_label)
             }
-        
+
             return jsonify(response), 200
-        except Exception as e:
-            return jsonify({'error': str(e)}), 400
+        else:
+            return jsonify({'error': 'Invalid request'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
